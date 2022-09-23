a20813fe04e3b33bb0dd68bf12455499f392d6ef
qverg <88112919+qverg@users.noreply.github.com>
2022-09-23 15:19:25 +0200

    updated compilation settings

--- parakeet/parakeet-gui/compile.bat
+++ parakeet/parakeet-gui/compile.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..\src
-g++ .\main.cpp .\board.cpp .\move.cpp .\engine.cpp .\utility.cpp .\types\movecounter.cpp -o ..\bin\parakeet
+g++ .\main.cpp .\board.cpp .\move.cpp .\engine.cpp .\utility.cpp .\timer.cpp .\types\movecounter.cpp -o ..\bin\parakeet
 cd ..\parakeet-gui
