8d40a0564722f5ce6752ab3708fdc696955b3147
qverg <88112919+qverg@users.noreply.github.com>
2023-01-19 20:55:31 +0100

    now calling the executable parakeet.out instead of parakeet

--- parakeet/src/compile.sh
+++ parakeet/src/compile.sh
@@ -1,1 +1,1 @@
-g++ main.cpp board.cpp move.cpp engine.cpp utility.cpp timer.cpp types/movecounter.cpp -o ../bin/parakeet
+g++ main.cpp board.cpp move.cpp engine.cpp utility.cpp timer.cpp types/movecounter.cpp -o ../bin/parakeet.out
